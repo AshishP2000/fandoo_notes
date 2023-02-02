3bf0baca230139092c76e9c5a3bab88cc6b08f10
Ashish <ashish2000patil@gmail.com>
2023-02-02 10:31:07 +0530

    [Ashish]Added: redis

--- note/redis_note.py
+++ note/redis_note.py
@@ -16,32 +16,21 @@ class RedisNote:
 
     def get(self, user_id):
         key = f'note_{user_id}'
-        payload = self.redis.getter(key)
+        payload = self.redis.getter(key[5:6])
         return payload
-
-    # def update(self, notes, user_id):
-    #     note_id = str(f'user_{notes.get("id")}_note')
-    #     print(note_id)
-    #     notes_dict = self.get(user_id)
-    #     note = notes_dict.get(note_id)
-    #     if note is not None:
-    #         notes_dict.update({note_id: notes})
-    #         print(notes_dict)
-    #         self.redis.setter(user_id, json.dumps(notes_dict))
 
     def delete(self, user_id, note_id):
         notes_dict = self.get(user_id)
         note = notes_dict.get(str(f'note_{note_id}'))
-        print(note)
         if note is not None:
             notes_dict.pop(str(f'note_{note_id}'))
             self.redis.setter(user_id, json.dumps(notes_dict))
 
 
-if __name__ == '__main__':
-    r = RedisNote()
+# if __name__ == '__main__':
+#     r = RedisNote()
     # r.save({'id': 2, 'note_title': 'something', 'note_body': 'hi to everyone', 'user': 2}, 2)
-    r.delete(2, 2)
-    print(r.get(2))
-#     # print(r.redis.redis_client.keys("*"))
-# # user_1_note : {"id":1,"title_note":"some",""}
+    # r.delete(2, 2)
+    # print(r.get(1))
+    # print(r.redis.redis_client.keys("*"))
+# user_1_note : {"id":1,"title_note":"some",""}
--- note/redis_service.py
+++ note/redis_service.py
@@ -2,24 +2,23 @@ import json
 import logging
 
 import redis
-from django.conf import settings
 
 logging.basicConfig(filename='fundoo_note.log', level=logging.DEBUG)
 logger = logging.getLogger()
+
 
 # redis_client = redis.Redis(**settings.REDIS_CONFIG)
 # redis_client = redis.Redis(**{"host": 'localhost', "port": 6379, "db": 0})
 
 
 class RedisCrud:
-
     redis_client = redis.Redis(**{"host": 'localhost', "port": 6379, "db": 0})
 
     def getter(self, key):
         """
         This function get the data of key from redis server
         """
-        data = self.redis_client.get(int(key[5:6]))
+        data = self.redis_client.get(key)
         return json.loads(data) if data else {}
 
     def setter(self, key, value):
